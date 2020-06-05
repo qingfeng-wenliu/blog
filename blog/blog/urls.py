a9b25ac886a2f2571f85fcb98662775b38954630
qingfeng-wenliu <415412309@qq.com>
2020-06-05 10:20:24 +0800

    test

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def log(request):
+    return HttpResponse('test')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', log),
 ]
